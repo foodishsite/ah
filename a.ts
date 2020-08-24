fe86cd0adf7ac73109bcbc5b321ea39125dcf1ec
Foodish <68122826+foodishsite@users.noreply.github.com>
2020-08-24 20:55:40 +0530

    Add files via upload

---
+++ a.ts
@@ -0,0 +1,12 @@
+var a: string[]= ["*", "*", "*", "*", "*"]
+
+a.forEach(function(h){
+    document.write(h + "<br>")
+})
+
+n
+d
+e
+r
+s
+
---
+++ h.cs
@@ -0,0 +1,19 @@
+using System;
+class HelloWorld {
+  static void Main() {
+    string h[] = {"*", "*", "*", "*", "*", "*", "*"}
+    for(a in h)
+    {
+        Console.WriteLine(a)
+    }
+  }
+}
+
+e
+j 
+l 
+s 
+b 
+e 
+r 
+g 
